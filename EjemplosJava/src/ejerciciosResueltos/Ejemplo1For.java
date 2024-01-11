94d81a4d111e635272ba30ad986688433410d0e8
victorpb <victorpb@.local>
2024-01-11 14:08:23 +0100

    comit inicial

---
+++ EjemplosJava/src/ejerciciosResueltos/Ejemplo1For.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+package ejerciciosResueltos;
+
+/**
+ * 
+ */
+public class Ejemplo1For {
+
+	/**
+	 * 
+	 */
+	public Ejemplo1For() {
+		// TODO Tengo que dar cuerpo a este constructor
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+
+		int i;
+		 for(i=1; i<=10;i++)
+		 System.out.println(i + " ");
+		
+	}
+
+	
+	//TODO mañana acordarse de hacer un nuevo metodo
+	
+	
+}
---
+++ EjemplosJava/src/ejerciciosResueltos/EjemploDepuracion.java
@@ -0,0 +1,16 @@
+package ejerciciosResueltos;
+
+/*
+ * 
+* programa que muestra los números del 1 al 10
+*
+ */
+public class EjemploDepuracion {
+	
+	 public static void main(String[] args) {
+	 int i;
+	 for(i=1; i<=10;i++)
+	 System.out.println(i + " ");
+	 }
+	}
+
